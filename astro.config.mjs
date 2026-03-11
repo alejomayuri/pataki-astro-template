db85028e50dd8f4ce5e554b2bfee9cac2955261b
Alejo Mayuri <alejomayuri@gmail.com>
2026-03-11 14:39:49 -0500

    upload web

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@tailwindcss/vite';
 
 export default defineConfig({
-  integrations: [], // Leave this array empty or for other integrations
+  integrations: [tailwind()], // Leave this array empty or for other integrations
   vite: {
     plugins: [tailwind()],
   },
